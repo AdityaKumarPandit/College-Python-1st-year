7da4236603a97c0b84c938b99a25426378acda10
AdityaKumarPandit <aditya.26bce10029@vitbhopal.ac.in>
2026-09-22 15:20:21 +0530

    22 sept 2026 1st commit

---
+++ sq_root.py
@@ -0,0 +1,9 @@
+n = int(input("Enter a positive integer: "))
+
+if n < 0:
+    print("Square root is not defined for a negative number.")
+else:
+    i = 0
+    while i * i <= n:
+        i += 1
+    print("Integer square root =", i - 1)
